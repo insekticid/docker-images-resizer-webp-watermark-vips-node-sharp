3b42c489737f415f1a4daa779d13c26d11a524e9
insekticid <insekticid@exploit.cz>
2017-10-08 20:30:32 +0200

    rewritten image generating

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,12 @@
 // Include Gulp & Tools We'll Use
 var gulp = require('gulp');
 
-var imageResize = require('gulp-image-resize');
-const imagemin = require('imagemin');
-var imageminWebp = require('imagemin-webp');
 var debug = require('gulp-debug');
 var changed = require("gulp-changed");
 var watermark = require("gulp-watermark");
-var os = require('os');
-var parallel = require('concurrent-transform');
-var pipes = require('gulp-pipes');
-var webp = require('gulp-webp');
+
 var runSequence = require('run-sequence');
 var responsive = require('gulp-responsive');
-var rename = require("gulp-rename");
 
 // Config
 var config = {
@@ -26,7 +19,7 @@ var config = {
 
 var resizeImageTasks = [];
 
-['125x125', '1024x1024','300x300', '270x190'].forEach(function(size) {
+['1024x1024'].forEach(function(size) {
     var sizes  = size.split('x');
     var width  = sizes[0];
     var height = sizes[1];
@@ -43,25 +36,108 @@ var resizeImageTasks = [];
     var resizeImageTask = 'resize_' + size;
 
     gulp.task(resizeImageTask, function() {
-        return gulp.src(config.source_images_path + "/*/**/*.{jpg,png}", { base: config.source_images_path })
+        gulp.src(config.source_images_path + "/*/**/*.{jpg,png}", { base: config.source_images_path })
             .pipe(changed(config.images_path + '/' + size + '/', {extension: '.jpg'}))
-            .pipe(debug())
-            .pipe(parallel(
-                imageResize({
-                    width:  width,
-                    height: height,
-                    upscale: true,
-                    format: 'jpg',
-                    noProfile: true
-                }),
-                os.cpus().length)
-            )
+            .pipe(responsive({
+                '*': [
+                    // {
+                    //     withoutEnlargement: false,
+                    //     width:  width,
+                    //     height: height,
+                    //     rename: {
+                    //         extname: ".webp"
+                    //     },
+                    //     format: 'webp'
+                    // },
+                    {
+                        withoutEnlargement: false,
+                        width:  125,
+                        height: 125,
+                        //max: true,
+                        rename: function (path) {
+                            path.dirname = "125x125/" + path.dirname;
+                            path.extname = ".jpg";
+                            return path;
+                        },
+                        format: 'jpeg'
+                    }
+                ]
+            }, {
+                // global quality for all images
+                //quality: 50,
+                withMetadata: false,
+                errorOnUnusedImage: false
+            }))
             .pipe(watermark({
                 image: "watermark.png",
-                resize: watemarkResize,
+                resize: '50%',
                 gravity: 'Center'
             }))
-            .pipe(gulp.dest(config.images_path + '/' + size + '/'))
+            .pipe(gulp.dest(config.images_path + '/'));
+
+        return gulp.src(config.source_images_path + "/*/**/*.{jpg,png}", { base: config.source_images_path })
+            .pipe(changed(config.images_path + '/' + size + '/', {extension: '.jpg'}))
+            .pipe(responsive({
+                '*': [
+                    // {
+                    //     withoutEnlargement: false,
+                    //     width:  width,
+                    //     height: height,
+                    //     rename: {
+                    //         extname: ".webp"
+                    //     },
+                    //     format: 'webp'
+                    // },
+                    {
+                        withoutEnlargement: false,
+                        width:  270,
+                        height: 190,
+                        ignoreAspectRatio: true,
+                        rename: function (path) {
+                            path.dirname = "270x190/" + path.dirname;
+                            path.extname = ".jpg";
+                            return path;
+                        },
+                        format: 'jpeg'
+                    },
+                    {
+                        withoutEnlargement: false,
+                        width:  300,
+                        height: 300,
+                        //max: true,
+                        rename: function (path) {
+                            path.dirname = "300x300/" + path.dirname;
+                            path.extname = ".jpg";
+                            return path;
+                        },
+                        format: 'jpeg'
+                    },
+                    {
+                        withoutEnlargement: false,
+                        width:  1024,
+                        height: 1024,
+                        max: true,
+                        rename: function (path) {
+                            path.dirname = "pictures/" + path.dirname;
+                            path.extname = ".jpg";
+                            return path;
+                        },
+                        format: 'jpeg'
+                    }
+                ]
+            }, {
+                // global quality for all images
+                //quality: 50,
+                withMetadata: false,
+                errorOnUnusedImage: false,
+                errorOnUnusedConfig: false
+            }))
+            .pipe(watermark({
+                image: "watermark.png",
+                resize: '221x72',
+                gravity: 'Center'
+            }))
+            .pipe(gulp.dest(config.images_path + '/'))
     });
 
     resizeImageTasks.push(resizeImageTask);
@@ -70,41 +146,29 @@ var resizeImageTasks = [];
 
 gulp.task('resize_images', resizeImageTasks);
 
-gulp.task('generate_webp', function() {
-    return gulp.src(config.images_path + "/*/**/*.{jpg,png}", { base: config.images_path })
-        .pipe(changed(config.images_path, {extension: '.webp'}))
-        .pipe(responsive({
-            '*/*/**/*.jpg': {
-                withoutEnlargement: true
-            }
-        }, {
-            // global quality for all images
-            quality: 50,
-            format: 'webp',
-            errorOnUnusedImage: false
-        }))
-        .pipe(rename({
-            extname: ".webp"
-        }))
-        .pipe(gulp.dest(config.images_path))
-});
-
 gulp.task('recompress', function() {
     return gulp.src(config.source_images_path + "/*/**/*.{jpg,png}", { base: config.source_images_path })
         .pipe(changed(config.images_path + '/pictures/', {extension: '.jpg'}))
-        .pipe(debug())
-        .pipe(
-            parallel(imageResize({
-                noProfile: true
-            }),
-            os.cpus().length)
+        .pipe(responsive({
+                '**/*.{jpg,png}': [
+                    {
+                        format: 'jpeg'
+                    }
+                ]
+            }, {
+                // global quality for all images
+                //quality: 50,
+                withMetadata: false,
+                errorOnUnusedImage: false,
+                errorOnUnusedConfig: false
+            })
         )
         .pipe(gulp.dest(config.source_images_path))
 });
 
 gulp.task('default', function(done) {
-    runSequence('generate_webp', function () {
-    //runSequence('recompress', 'resize_images', 'generate_webp', function () {
+    //runSequence('resize_images', function () {
+    runSequence('recompress', 'resize_images', function () {
         console.log('Finished');
         done();
     });
