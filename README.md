d8ed65736c81e0ac9153cafba51f80c4b0b3a7f4
insekticid <insekticid@exploit.cz>
2017-10-11 17:02:10 +0200

    gulp changed not working in docker environment, changed way how to deal with pictures - from unprocessed folder

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 How to use
 -----------
 
-1. edit docker-compose.yml and modify `original` and `generated` paths to your images folder
-   1. where `original` folder is your folder with `jpg` or `png` pictures (there is hardcoded `*.{jpg,png}` filter)
+1. edit docker-compose.yml and modify `unprocessed`, `original` and `generated` paths to your image folders
+   1. where `unprocessed` folder is your folder with new pictures to process
    1. where `generated` folder is your folder where new images will be saved
+   1. where `original` folder is your folder with original pictures without watermark
 1. if you would like to add watermark, edit path to your `watermark.png` in docker-compose.yml
 1. you can edit output picture sizes in `gulpfile.js` (125x125, 300x300, 270x190, 1024x1024) 
-1. this script will regenerate pictures if original has changed
-1. first time run will recompress your original images. you can disabled it via modify `gulpfile.js` and modify runSequence to only `resize_images`
 
 How to run
 ------------
--- gulpfile.js
+++ gulpfile.js
@@ -4,151 +4,61 @@
 var gulp = require('gulp');
 
 var debug = require('gulp-debug');
-var changed = require("gulp-changed");
 var watermark = require("gulp-watermark");
 
 var runSequence = require('run-sequence');
 var responsive = require('gulp-responsive');
 
+var rm = require('gulp-rimraf');
+
 // Config
 var config = {
-    source_images_path      : 'original',
-    images_path             : 'generated',
+    source_images_path: 'unprocessed',
+    original_images_path: 'original',
+    images_path: 'generated'
     //images_upload_path      : 'web/uploads'
 };
 
-var resizeImageTasks = [];
-
-['1024x1024'].forEach(function(size) {
-    var sizes  = size.split('x');
-    var width  = sizes[0];
-    var height = sizes[1];
-    var watemarkResize = '221x72';
-
-    if (size === '1024x1024') {
-        size = 'pictures';
-    }
-
-    if (width <= 250) {
-        watemarkResize = (width * 0.7) + 'x' + (width * 0.7);
-    }
-
-    var resizeImageTask = 'resize_' + size;
-
-    gulp.task(resizeImageTask, function() {
-        gulp.src(config.source_images_path + "/*/**/*.{jpg,png}", { base: config.source_images_path })
-            .pipe(changed(config.images_path + '/' + size + '/', {extension: '.jpg'}))
-            .pipe(responsive({
-                '**/*.{jpg,png}': [
-                    // {
-                    //     withoutEnlargement: false,
-                    //     width:  width,
-                    //     height: height,
-                    //     rename: {
-                    //         extname: ".webp"
-                    //     },
-                    //     format: 'webp'
-                    // },
-                    {
-                        withoutEnlargement: false,
-                        width:  125,
-                        height: 125,
-                        //max: true,
-                        rename: function (path) {
-                            path.dirname = "125x125/" + path.dirname;
-                            path.extname = ".jpg";
-                            return path;
-                        },
-                        format: 'jpeg'
-                    }
-                ]
-            }, {
-                // global quality for all images
-                //quality: 50,
-                withMetadata: false,
-                errorOnUnusedImage: false
-            }))
-            .pipe(watermark({
-                image: "watermark.png",
-                resize: '50%',
-                gravity: 'Center'
-            }))
-            .pipe(gulp.dest(config.images_path + '/'));
-
-        return gulp.src(config.source_images_path + "/*/**/*.{jpg,png}", { base: config.source_images_path })
-            .pipe(changed(config.images_path + '/' + size + '/', {extension: '.jpg'}))
-            .pipe(responsive({
-                '**/*.{jpg,png}': [
-                    // {
-                    //     withoutEnlargement: false,
-                    //     width:  width,
-                    //     height: height,
-                    //     rename: {
-                    //         extname: ".webp"
-                    //     },
-                    //     format: 'webp'
-                    // },
-                    {
-                        withoutEnlargement: false,
-                        width:  270,
-                        height: 190,
-                        ignoreAspectRatio: true,
-                        rename: function (path) {
-                            path.dirname = "270x190/" + path.dirname;
-                            path.extname = ".jpg";
-                            return path;
-                        },
-                        format: 'jpeg'
+const resizeImages = function () {
+    gulp.src(config.source_images_path + "/*/**/*.{jpg,png}", {base: config.source_images_path})
+        .pipe(responsive({
+            '**/*.{jpg,png}': [
+                // {
+                //     withoutEnlargement: false,
+                //     width:  width,
+                //     height: height,
+                //     rename: {
+                //         extname: ".webp"
+                //     },
+                //     format: 'webp'
+                // },
+                {
+                    withoutEnlargement: false,
+                    width: 125,
+                    height: 125,
+                    //max: true,
+                    rename: function (path) {
+                        path.dirname = "125x125/" + path.dirname;
+                        path.extname = ".jpg";
+                        return path;
                     },
-                    {
-                        withoutEnlargement: false,
-                        width:  300,
-                        height: 300,
-                        //max: true,
-                        rename: function (path) {
-                            path.dirname = "300x300/" + path.dirname;
-                            path.extname = ".jpg";
-                            return path;
-                        },
-                        format: 'jpeg'
-                    },
-                    {
-                        withoutEnlargement: false,
-                        width:  1024,
-                        height: 1024,
-                        max: true,
-                        rename: function (path) {
-                            path.dirname = "pictures/" + path.dirname;
-                            path.extname = ".jpg";
-                            return path;
-                        },
-                        format: 'jpeg'
-                    }
-                ]
-            }, {
-                // global quality for all images
-                //quality: 50,
-                withMetadata: false,
-                errorOnUnusedImage: false,
-                errorOnUnusedConfig: false
-            }))
-            .pipe(watermark({
-                image: "watermark.png",
-                resize: '221x72',
-                gravity: 'Center'
-            }))
-            .pipe(gulp.dest(config.images_path + '/'))
-    });
+                    format: 'jpeg'
+                }
+            ]
+        }, {
+            // global quality for all images
+            //quality: 50,
+            withMetadata: false,
+            errorOnUnusedImage: false
+        }))
+        .pipe(watermark({
+            image: "watermark.png",
+            resize: '50%',
+            gravity: 'Center'
+        }))
+        .pipe(gulp.dest(config.images_path + '/'));
 
-    resizeImageTasks.push(resizeImageTask);
-});
-
-
-gulp.task('resize_images', resizeImageTasks);
-
-gulp.task('recompress', function() {
-    return gulp.src(config.source_images_path + "/*/**/*.{jpg,png}", { base: config.source_images_path })
-        .pipe(changed(config.images_path + '/pictures/', {extension: '.jpg'}))
+    return gulp.src(config.source_images_path + "/*/**/*.{jpg,png}", {base: config.source_images_path})
         .pipe(responsive({
                 '**/*.{jpg,png}': [
                     {
@@ -163,12 +73,99 @@ gulp.task('recompress', function() {
                 errorOnUnusedConfig: false
             })
         )
-        .pipe(gulp.dest(config.source_images_path))
+        .pipe(gulp.dest(config.original_images_path))
+        .pipe(responsive({
+            '**/*.{jpg,png}': [
+                // {
+                //     withoutEnlargement: false,
+                //     width:  width,
+                //     height: height,
+                //     rename: {
+                //         extname: ".webp"
+                //     },
+                //     format: 'webp'
+                // },
+                {
+                    withoutEnlargement: false,
+                    width: 270,
+                    height: 190,
+                    ignoreAspectRatio: true,
+                    rename: function (path) {
+                        path.dirname = "270x190/" + path.dirname;
+                        path.extname = ".jpg";
+                        return path;
+                    },
+                    format: 'jpeg'
+                },
+                {
+                    withoutEnlargement: false,
+                    width: 300,
+                    height: 300,
+                    //max: true,
+                    rename: function (path) {
+                        path.dirname = "300x300/" + path.dirname;
+                        path.extname = ".jpg";
+                        return path;
+                    },
+                    format: 'jpeg'
+                },
+                {
+                    withoutEnlargement: false,
+                    width: 1024,
+                    height: 1024,
+                    max: true,
+                    rename: function (path) {
+                        path.dirname = "pictures/" + path.dirname;
+                        path.extname = ".jpg";
+                        return path;
+                    },
+                    format: 'jpeg'
+                }
+            ]
+        }, {
+            // global quality for all images
+            //quality: 50,
+            withMetadata: false,
+            errorOnUnusedImage: false,
+            errorOnUnusedConfig: false
+        }))
+        .pipe(watermark({
+            image: "watermark.png",
+            resize: '221x72',
+            gravity: 'Center'
+        }))
+        .pipe(gulp.dest(config.images_path + '/'));
+};
+
+gulp.task('resize_images', resizeImages);
+
+gulp.task('recompress', function () {
+    return gulp.src(config.original_images_path + "/*/**/*.{jpg,png}", {base: config.original_images_path})
+        .pipe(responsive({
+                '**/*.{jpg,png}': [
+                    {
+                        format: 'jpeg'
+                    }
+                ]
+            }, {
+                // global quality for all images
+                //quality: 50,
+                withMetadata: false,
+                errorOnUnusedImage: false,
+                errorOnUnusedConfig: false
+            })
+        )
+        .pipe(gulp.dest(config.original_images_path));
 });
 
-gulp.task('default', function(done) {
-    //runSequence('resize_images', function () {
-    runSequence('recompress', 'resize_images', function () {
+gulp.task('finish', function () {
+    return gulp.src(config.source_images_path + "/*/**/*.{jpg,png}", {base: config.source_images_path})
+        .pipe(rm());
+});
+
+gulp.task('default', function (done) {
+    runSequence('resize_images', 'finish', function () {
+    //runSequence('recompress', 'resize_images', 'finish', function () {
         console.log('Finished');
         done();
     });
